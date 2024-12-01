37cadba3001d34b4d0c427cba10a98e291fa84cc
Aguszar <agustinzarzur4@gmail.com>
2024-11-30 21:57:13 -0300

    peliculas y movies

--- movies.js
+++ movies.js
@@ -3,7 +3,7 @@ movie_id=[
     "<iframe src='https://mega.nz/embed/MOlwlbQS#0E3o-96MPtodbn3iG3qpy7z6XbqtJrf2aWMf_m-7zI0' allowfullscreen ></iframe>",
     "<iframe src='https://mega.nz/embed/aGwWmbQK#sD7X3pS7MzwzR9mFWSf59e4r7hvmrlQocfp7HseDRms' allowfullscreen ></iframe>",
     "<iframe src='https://mega.nz/embed/ResQBBxI#qYCOVnRQABZtL42LjegPJ13Vu6UoehIo3rKUgj4oaiA' allowfullscreen ></iframe>",
-    "1qWz5BVAExV8piQAGkGLgVI9XoIRhryeh" , "1w1mkPsX1iQ1y2vdw7cBRwx7zQyZmO6U8",
+    "1qWz5BVAExV8piQAGkGLgVI9XoIRhryeh" ,
     "<iframe src='https://mega.nz/embed/FD8VXT6R#BeuT2q7nX--mOLOW3mXm11TzysDPP8MyrbYYOan3hqU' allowfullscreen ></iframe>",
     "<iframe src='https://mega.nz/embed/5bdnVZ7D#8NLbEXSTR_saWZQ0eVjMAAtNUxaPYbFTX80wIr6zhxQ' allowfullscreen ></iframe>", // stone
     "<iframe src='https://mega.nz/embed/0DcQ1S6C#FHO8noTkRsE-38CV8jI_I9F8FLswCw0rsfzdCAFc0dI' allowfullscreen ></iframe>" // secrets
@@ -14,17 +14,23 @@ movie_id=[
     ,"<iframe src='https://mega.nz/embed/FbtzCZ7Y#FYfBaRSR9M3WeBlW7V3u2UVuDpzrT69DKvfHrN9JmQc' allowfullscreen ></iframe>" // dead 1
     ,"<iframe src='https://mega.nz/embed/nzR3VAIA#W8XnRTZl3mfIMvPz-Kvw5tPzGW2BuwjlgbBhnskuhOo' allowfullscreen ></iframe>" // dead 2
     ,"<iframe src='https://mega.nz/embed/C3BWwCBB#w_bvhpG_J6pQrdF5XaFdGa72Viqj_xvndUzi6kdYMnk' allowfullscreen ></iframe>" // prince of egypt
-    ,"<iframe src='https://mega.nz/embed/Sn5gECBb#I8ALSYKyJbQuYLRXCXZ5_g0nDJkucER6REuS8GCpUis' allowfullscreen ></iframe>" // shrek 1
     ,"<iframe src='https://terabox.com/sharing/embed?surl=PZeX2EqlOKS10ZL7CuNKGw&resolution=720&autoplay=true&mute=false&uk=4401448085563&fid=876330162364812&slid=' allowfullscreen></iframe>" // my cousin vinny
     ,"<iframe src='https://terabox.com/sharing/embed?surl=AGbyf08G92FkY7tyCupK0A&resolution=1080&autoplay=true&mute=false&uk=4401448085563&fid=194448470385188&slid=' allowfullscreen></iframe>" // pleasentville
     ,"<iframe src='https://terabox.com/sharing/embed?surl=Te4PtGoqPZEIfqt2XKcAJw&resolution=720&autoplay=true&mute=false&uk=4401448085563&fid=669421472600065&slid=' allowfullscreen></iframe>"  // Rocky 
-    ,"<iframe src='htps://mega.nz/embed/bqAHUCDb#MlF_3Z-O1QInhKWtMCY8aopsDJqpdSpuYXlSEEB62rA' allowfullscreen ></iframe>" // metegol
     ,"<iframe src='https://terabox.com/sharing/embed?surl=du1Gk_kxxfo-XSkFgq_eEg&resolution=720&autoplay=true&mute=false&uk=4401448085563&fid=866958939395770&slid=' allowfullscreen></iframe>" // spiderman 1 2002
     ,"<iframe src='https://terabox.com/sharing/embed?surl=KwdHWprL46jJwZtuH1XLTQ&resolution=720&autoplay=true&mute=false&uk=4401448085563&fid=211912439592749&slid=' allowfullscreen></iframe>" // Schindlers List
     ,"<iframe src='https://terabox.com/sharing/embed?surl=K-8XN9SkHCn7eIIjNSuVQA&resolution=1080&autoplay=true&mute=false&uk=4401448085563&fid=216593716813281&slid=' allowfullscreen></iframe>" // white chicks
-
 ]
 
-movie_name=["Barbarian","Inglourious Basterds", "Ice Age 2", "District 9", "Nacho Libre", "Shrek 2 (esp)", "Django Unchained",  
+
+movie_name=["Barbarian","Inglourious Basterds", "Ice Age 2", "District 9", "Nacho Libre", , "Django Unchained",  
         "H.P and T.S.Stone", "H.P and T.C.o.S", "H.P & the Prisoner Akzaban", "H.P & Goblet Fire" ,"H.P & the Order of the phoenix", "H.P & half blood prince", "H.P & Deadly Hallows (1)","H.P & Deadly Hallows (2)"
-        ,"Prince of Egypt", "Shrek 1 (esp)", "My cousin Vinny", "Pleasantville", "Rocky", " Metegol", "SpiderMan (2002)", "Schindler's List", "White Chicks"]
+        ,"Prince of Egypt", "My cousin Vinny", "Pleasantville", "Rocky", "SpiderMan (2002)", "Schindler's List", "White Chicks"]
+
+peli_id = [
+    "<iframe src='https://mega.nz/embed/Sn5gECBb#I8ALSYKyJbQuYLRXCXZ5_g0nDJkucER6REuS8GCpUis' allowfullscreen ></iframe>" // shrek 1
+    ,"1w1mkPsX1iQ1y2vdw7cBRwx7zQyZmO6U8" // shrek 2
+    ,"<iframe src='https://mega.nz/embed/bqAHUCDb#MlF_3Z-O1QInhKWtMCY8aopsDJqpdSpuYXlSEEB62rA' allowfullscreen ></iframe>" // metegol
+]
+
+peli_nombre = ["Shrek 1", "Shrek 2 ", "Metegol"]
